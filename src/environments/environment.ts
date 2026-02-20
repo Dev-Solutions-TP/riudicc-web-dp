b392d4171be49fbd4cd8d39dc7e2b4ebe22f0e95
PabloTorres <ptorresp@ups.edu.ec>
2026-02-20 17:12:54 -0500

    UPD URL SERVER

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,7 @@ export const environment = {
     // baseUrl: 'http://172.16.248.167:3000/api',
     // baseUrl: 'http://192.168.100.236:3000/api',
     // baseUrl: 'http://192.168.100.245:4200/api'
-    baseUrl: 'https://riudicc-server.onrender.com/api'
+    // baseUrl: 'https://riudicc-server.onrender.com/api'
+    baseUrl: 'https://api.72.62.81.187.sslip.io/api',
+
 };
